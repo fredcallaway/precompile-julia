6de8f99f9e98cdbd15a45fffebb494f37c2ea190
fredcallaway <fredc@llaway.com>
2022-09-19 20:59:34 -0400

    DataFramesMeta

--- make_image.jl
+++ make_image.jl
@@ -1,6 +1,8 @@
 #!/usr/local/bin/julia16
 using Pkg
 
+path = isempty(ARGS) ? "$VERSION.so" : ARGS[1]
+
 packages = """
 Distributions
 Parameters
@@ -13,12 +15,16 @@ JSON
 StatsFuns
 CSV
 DataFrames
+DataFramesMeta
 Printf
 StatsBase
 StatsPlots
 AxisKeys
 """
 
+Pkg.add(url="https://github.com/BioTurboNick/AbbreviatedStackTraces.jl")
+
 Pkg.add(["PackageCompiler"; split(packages)])
+Pkg.update(["PackageCompiler"; split(packages)])
 using PackageCompiler
-@time create_sysimage(Symbol.(split(packages)), sysimage_path="sysimage.so", precompile_execution_file="precompile.jl")
+@time create_sysimage(Symbol.(split(packages)), sysimage_path=path, precompile_execution_file="precompile.jl")
--- precompile.jl
+++ precompile.jl
@@ -1,9 +1,18 @@
 using CSV
 using DataFrames
+using DataFramesMeta
 df = CSV.read("test.csv", DataFrame)
 CSV.write("test.csv", df)
 
+@chain df begin
+    @rtransform :z = :a + :b
+    @rsubset :z > 3
+    @by(:c, :ma = sum(:a))
+    # @select :ma
+end
+
 using StatsPlots
+ENV["GKSwstype"] = "nul"
 display(plot(rand(5), rand(5)))
 
 using Distributions
@@ -20,3 +29,6 @@ end
 
 using AxisKeys
 using ProgressMeter
+@showprogress map(1:100) do i
+    i+2
+end
